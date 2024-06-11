1a78dda452a12e2308cd7ee56fb9155bed62f753
m1Codigover23 <david.humberto@gmail.com>
2024-06-11 14:37:31 -0600

    Creacion de la interfaz

---
+++ calculadora.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>OPERACIONES ARITMETICAS</h1>
+<form action="operaciones.php" method="post">
+    <div class="card">
+      <img src="" alt="">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text"></p>
+        <div class="text-warning">
+        <div class="form-group">
+        <label for="exampleInput">Dato 1</label>
+        <input type="text" class="form-control" id="d1" placeholder="introduce un valor numerico">
+        <br><br>
+        <label for="exampleInput">Dato 2</label>
+        <input type="text" class="form-control" id="d2" placeholder="introduce un valor numerico">
+        <br><br>
+        <input type="button" value="suma">
+        <?php 
+        
+
+        echo "hola esto es una modificación";  ?>
+         echo "Aqui se pondra las operaciones";  ?>
+    </div>
+        </div>
+        <a href="#" class="btn btn-primary pull-right"></a>
+      </div>
+    </div>
+
+
+</form>
+    
+</body>
+</html>
